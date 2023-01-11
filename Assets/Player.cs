0406260bbeb1501df15978bcb13e19371216becb
Drobz <tim.stenhammar@gmail.com>
2023-01-11 13:05:54 +0100

    Update Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,19 +8,46 @@ public class Player : MonoBehaviour
     public int money;
 
     //TUBE
-    public float filledTube = 15;
+    public bool inWater;
+    public float maxTube = 15;
     public float tubeRemaining;
+    public bool isFilled;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxTube = 15;
+        speed = 2;
+        tubeRemaining = maxTube;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("filled tube is" + filledTube);
+        if (inWater)
+        {
+            tubeRemaining -= 1 * Time.deltaTime;
+        }
+        else if (!inWater && tubeRemaining != maxTube && tubeRemaining <= maxTube)
+        {
+            tubeRemaining += 1 * Time.deltaTime;
+        }
+
+
+        if(tubeRemaining <= 0)
+        {
+            die();
+        }
+
+
+
+        print("filled tube is" + maxTube);
         print("speed is " + speed);
+    }
+
+    public void die()
+    {
+        print("you died.");
     }
 }
