18ca182567d1b9864305d956b10d3a8420d51400
JaggedSwords <97683126+JaggedSwords@users.noreply.github.com>
2023-01-16 13:07:29 +0100

    sdfghjkl

---
+++ Assets/Scripts/LoadSceneAfterTime.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAfterTime : MonoBehaviour
+{
+    [SerializeField]
+    int buildindex;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    [SerializeField]
+    private float delayBeforeLoading = 10f;
+    [SerializeField]
+    private float timeElapsed;
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(buildindex);
+        }
+    }
+}
