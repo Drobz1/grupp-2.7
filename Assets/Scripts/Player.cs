1de54be28161c7f4388689471d9d55485395ca8c
Drobz <tim.stenhammar@gmail.com>
2023-01-18 14:50:18 +0100

    Revert "o"
    This reverts commit cc921be91a77899f4773a4dca9e4edbab3177591.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -47,7 +47,6 @@ public class Player : MonoBehaviour
 
     public bool deathscreenspawned = false;
     PlasticScore PlasticScore;
-    JeweleryScore JeweleryScore;
     public GameObject deathscreenprefab;
     float deathTimer = 99999999;
 
@@ -73,7 +72,6 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         //Ovanför är movement
 
-        JeweleryScore = FindObjectOfType<JeweleryScore>();
         PlasticScore = FindObjectOfType<PlasticScore>();
         maxTube = 15;
         speed = 2;
@@ -159,7 +157,6 @@ public class Player : MonoBehaviour
         if(tubeRemaining <= 0)
         {
             PlasticScore.score = 0;
-            JeweleryScore.score = 0;
             if(!deathscreenspawned)
             {
                 Instantiate(deathscreenprefab, new Vector3(0, 0, 0), Quaternion.identity);
